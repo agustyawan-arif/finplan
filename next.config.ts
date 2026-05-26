ff70eceb708b00e51e5b936dbba6a1989e873244
agustyawan-arif <arifagustyawan@gmail.com>
2026-05-26 22:08:19 +0700

    feat: complete Supabase-backed finance app MVP

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins: ['192.168.182.14', '10.15.1.20'],
+  allowedDevOrigins: ['192.168.182.14', '10.15.1.20', '172.20.10.5'],
 };
 
 export default nextConfig;
